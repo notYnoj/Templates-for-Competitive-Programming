6a90678e0bcd7dd1b9bac60b3221a8948a8efbc1
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-08-26 18:59:35 -0500

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 
 // Everything Else thats new :D
-template <typename T> //custom output stream operator
+template <typename T> //custom output stream operator for vector
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
     for (const auto& elem : vec) {
         os << elem << " ";
+    }
+    return os;
+}
+template <typename T, std::size_t N> //custom output stream operator for array
+std::ostream& operator<<(std::ostream& os, const T (&arr)[N]) {
+    for (std::size_t i = 0; i < N; ++i) {
+        os << arr[i] << " ";
     }
     return os;
 }

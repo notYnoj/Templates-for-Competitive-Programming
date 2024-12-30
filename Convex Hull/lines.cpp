0cc72dedc1f2195e32c953defa92f51b6db3b4d2
Jonathan <jjalvarado14@cps.edu>
2024-12-29 20:28:35 -0600

    yeah?

--- Convex Hull/lines.cpp
+++ Convex Hull/lines.cpp
@@ -1,1 +1,19 @@
+template <typename type_T>
+struct ConvexHull_Lines{
+    struct line{
+        type_T m;
+        type_T b;
+        Line(type_T slope, type_T y_intercept): m(slope), b(y_intercept) {}
+    };
+    //maintain a multiset of lines such that we can compare lines
+    
+    multiset<line> lines;
+    bool redundant{
 
+    }
+    void add_line(T m, T b){
+        lines.insert(line(m, b));
+    }
+
+
+};

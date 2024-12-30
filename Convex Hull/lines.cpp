68b8999515e82707214954a84559ae6885d0180d
Jonathan <jjalvarado14@cps.edu>
2024-12-30 08:41:55 -0600

    Progress

--- Convex Hull/lines.cpp
+++ Convex Hull/lines.cpp
@@ -1,9 +1,22 @@
+const long long is_query = -(1LL<<30);
 template <typename type_T>
 struct ConvexHull_Lines{
     struct line{
         type_T m;
         type_T b;
         Line(type_T slope, type_T y_intercept): m(slope), b(y_intercept) {}
+        //comparator for when inside multiset
+        mutable function<const line*()> neighbor;
+        bool operator<(const line& rhs) const{
+            if(rhs.b != is_query){
+                return m<rhs.m;
+            }
+            const line* s = succ();
+            if(!s) return false;
+            type_T x = rhs.m;
+            //check if other is bigger m*x + b < ms*x+bs, b-bs<ms*x-m*x, x*(ms-m)
+            return b-s->b < (s->m-m) * x;
+        }
     };
     //maintain a multiset of lines such that we can compare lines
     
@@ -11,7 +24,7 @@ struct ConvexHull_Lines{
     bool redundant{
 
     }
-    void add_line(T m, T b){
+    void add_line(type_T m, type_T b){
         lines.insert(line(m, b));
     }
 

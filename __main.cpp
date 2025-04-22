169bad6c52bd8b13a1fe38894e58604b1f92beaa
Ynoj <117422590+notYnoj@users.noreply.github.com>
2025-04-21 20:52:55 -0500

    Update __main.cpp

--- __main.cpp
+++ __main.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define pb push_back
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
 using namespace std;
-
+    
 // Everything Else thats new :D
 template <typename T> //custom output stream operator for vector
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {

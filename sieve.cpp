ad4af3be8535ce2cddaebddb453a81e9d4b048fe
Ynoj <117422590+notYnoj@users.noreply.github.com>
2025-05-26 16:13:50 -0500

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -1,1 +1,14 @@
-
+vi primes;
+const int N = 1e5; //change if needed
+bool found[N];
+void sieve(){
+    fill(found, found+N+1, true);
+    for(int p = 2; p*p<=N; p++){
+        if(found[p]){
+            primes.pb(p);
+            for(int i = p*p; i<=N; i+=p){
+                found[i] = false;
+            }
+        }
+    }
+}

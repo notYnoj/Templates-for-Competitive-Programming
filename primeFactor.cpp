20c8c8ed8784ec370df52ef0761b921a0fd911dc
Ynoj <117422590+notYnoj@users.noreply.github.com>
2025-05-29 20:41:07 -0500

    Update primeFactor.cpp

--- primeFactor.cpp
+++ primeFactor.cpp
@@ -1,7 +1,7 @@
 const int N = 1e6+5;
 int found[N];
 void sieve(){
-    fill(found, found+N+1, -1);
+    fill(found, found+N, -1);
     for(int p = 2; p*p<=N; p++){
         if(found[p] == -1){
             found[p] = p;
@@ -13,8 +13,10 @@ void sieve(){
 }
 vi factor(int x){
     vi ret;
+    if(x<=1) return {1};
     while(x!=1){
         ret.pb(found[x]);
         x/=found[x];
     }
+    return ret;
 }

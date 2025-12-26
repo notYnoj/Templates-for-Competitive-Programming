4eb3438a547c3b72229dfa6f4311b164f6a2467e
Ynoj <117422590+notYnoj@users.noreply.github.com>
2025-12-26 11:04:36 -0600

    Update __main.cpp

--- __main.cpp
+++ __main.cpp
@@ -16,26 +16,6 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
     }
     return os;
 }
-/*namespace IO {
-    template<typename T>
-    std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
-        for (const auto& elem : vec) {
-            os << elem << " ";
-        }
-        return os;
-    }
-}
-using namespace IO;
-//for macos^^
-*/
-template <typename T, std::size_t N> //custom output stream operator for array
-std::ostream& operator<<(std::ostream& os, const T (&arr)[N]) {
-    for (std::size_t i = 0; i < N; ++i) {
-        os << arr[i] << " ";
-    }
-    return os;
-}
-
 
 int main(){
     ios_base::sync_with_stdio(false);

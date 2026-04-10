035acc05ec11df42d42cd11d482a2c94846a06bd
Ynoj <117422590+notYnoj@users.noreply.github.com>
2026-04-10 07:08:54 -0500

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -2,27 +2,51 @@ class DSU{
 public:
     vector<ll> size;
     vector<ll> parent;
+
     DSU(int n){
         size.resize(n);
         parent.resize(n);
-        for(ll i = 0; i<n; i++){
+        for(ll i = 0; i < n; i++){
             size[i] = 1LL;
-            parent[i] = i; 
+            parent[i] = i;
         }
     }
+
     ll getSize(int i){
-        return size[parent[i]];
+        return size[find(i)];  
     }
-    int find(int x){
-        return(parent[x] == x? x: parent[x] = find(x));
+    ll find(int x) {
+    // You need to call find on the PARENT, not on x again!
+        return (parent[x] == x ? x : parent[x] = find(parent[x])); 
     }
     void merge(ll x, ll y){
         ll xp = find(x);
         ll yp = find(y);
-        if(x>y){
-            swap(x,y);
-        }
+        if(xp == yp) return;
         size[xp] += size[yp];
-        parent[yp] = parent[xp];
+        parent[yp] = xp;  
+    }
+
+    int unique(){
+        vector<int> found(parent.size(), 0);
+        int count = 0;
+        for(int i = 0; i < parent.size(); i++){
+            int root = find(i);  
+            if(!found[root]){
+                count++;
+                found[root] = true;
+            }
+        }
+        return count;
+    }
+    void debug_parents(){
+        cout<<parent;
+    }
+    void debug_sizes(){
+        cout<<sizes;
+    }
+    void debug(){
+        cout<<parent<<'\n';
+        cout<<sizes<<'\n';
     }
 };
